0d2ddfa01590e8dcbe44d2f05092d73fb0f59174
aseshbasu <aseshbasu@gmail.com>
2023-05-24 22:00:32 +0530

    add card funtion done
    card_id works, all button have console outputs, block toolbar removed

--- app.js
+++ app.js
@@ -5,29 +5,33 @@ note_no_main=0
 //note_id="-1"
 all_notes="-1"
 
+textarea_html=`<textarea id="keyboard_textarea" placeholder="Write your text here. Click on Add New button below to add a new block."></textarea>
+`
+
+
+
 card_html=`
     <div class="card_toolbar"> 
-        <div class="card_toolbar_name">            
-            New note        
-        </div>
+        <div id="card_toolbar_name"> </div>
         
         <div class="card_toolbar_buttons">
-        <i  id="save" class="toolbar_icons fa-solid fa-save"></i>
-        <i  id="download" class="toolbar_icons fa-solid fa-download"></i>               
-        <i  id="space" class="toolbar_icons fa-solid fa-space"></i>               
-        <i  id="delete_card" class="toolbar_icons fa-solid fa-trash"></i>               
+            <i  id="save" class="toolbar_icons fa-solid fa-save"></i>
+            <i  id="download" class="toolbar_icons fa-solid fa-download"></i>               
+            <i  id="space" class="toolbar_icons fa-solid fa-space"></i>               
+            <i  id="delete_card" class="toolbar_icons fa-solid fa-trash"></i>               
         </div>
     </div>  
     
-    <div class="card">        
-        <div class="keyboard_block">
-            <textarea id="keyboard_textarea" placeholder="Write your text here. Click on Add New button below to add a new block."></textarea>
-            <div class="blocktoolbar">
-            <i  id="delete_block" class="blocktoolbar_icons fa-solid fa-trash"></i>
-            <i  id="duplicate_block" class="blocktoolbar_icons fa fa-files-o"></i>
-            </div>
-        </div>
-        
+    <div class="card">     
+
+    <div id="keyboard_block">   
+        ${textarea_html}
+    </div>
+    
+    <div id="insert_new_blocks_here"></div>
+
+
+
         <div class="toolbar_and_spacer">
             <div class="toolbar">
                 <div id="add_new_block">    
@@ -46,7 +50,7 @@ let rand_str=()=>{
 }
 
 let create_note=function(){
-    card_holder=document.createElement("div")
+    let card_holder=document.createElement("div")
     card_holder.classList.add("card_holder")
     card_holder.innerHTML=card_html
     main.appendChild(card_holder)///building the elements so that the next query selectors can find it 
@@ -56,9 +60,16 @@ let create_note=function(){
     let note_id=rand_str()
     console.log("new note created with note_no: "+note_no+" note_id: " +note_id)
 
-    add_new_block_btn= card_holder.querySelector("#add_new_block")
-    add_new_block_btn.addEventListener("click",
+    card_holder.querySelector("#card_toolbar_name").innerHTML="New note "+note_no
+
+    card_holder.querySelector("#add_new_block").addEventListener("click",
     function(){
+        let kb_block=document.createElement("div")
+        kb_block.setAttribute("id","keyboard_block")
+
+        kb_block.innerHTML=textarea_html
+        card_holder.querySelector("#insert_new_blocks_here").appendChild(kb_block)
+
         console.log("add_new_block done in note_no: "+note_no+" note_id: " +note_id)
     })
 
@@ -74,26 +85,15 @@ let create_note=function(){
         console.log("download done in note_no: "+note_no+" note_id: " +note_id)
     })
 
-    delete_card_btn= card_holder.querySelector("#delete_card")
-    delete_card_btn.addEventListener("click",
+    delete_card_btn= ""
+    card_holder.querySelector("#delete_card").addEventListener("click",
     function(){
+        card_holder.remove()
+        note_no_main-=1
         console.log("delete_card done in note_no: "+note_no+" note_id: " +note_id)
     })
 
-    delete_block_btn= card_holder.querySelector("#delete_block")
-    delete_block_btn.addEventListener("click",
-    function(){
-        console.log("delete_block done in note_no: "+note_no+" note_id: " +note_id)
-    })
-
-    duplicate_block_btn= card_holder.querySelector("#duplicate_block")
-    duplicate_block_btn.addEventListener("click",
-    function(){
-        console.log("duplicate_block done in note_no: "+note_no+" note_id: " +note_id)
-    })
-
-
-
+    
     //main.appendChild(card_holder)
 
 }
